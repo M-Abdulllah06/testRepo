fa9306e23de8ae4aa680c9ccfa1f292049d6d081
M Abdullah <ma5126170@gmail.com>
2026-05-25 17:08:35 +0500

    Q#2 modified

--- Question2.js
+++ Question2.js
@@ -2,15 +2,17 @@
 // -----------failed api-------------
 
 let url="https://meowfacts.herokuapp.com";
-async function retry(url,retries=3){
+async function retry(url,retries=3,delay=1000){
     try{
     let response=await fetch(url);
     let data=await response.json();
     console.log(data);
     }catch{
         if(retries>0){
+            setTimeout(() => {
             console.log("remaining retries are : ",retries);
-            return retry(url,retries-1);
+            return retry(url,retries-1,delay+1000);
+            }, delay); 
         }else{
             console.log("no more retry")
         }
